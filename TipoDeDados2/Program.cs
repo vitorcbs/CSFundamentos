11c3664f956d7729a816dbd468a3f0a36e38714d
Vitinho <vitorcbs18@gmail.com>
2023-05-04 22:45:04 -0300

    Add project files.

---
+++ TipoDeDados2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Atribuindo valores a double, float e decimal");
+Console.WriteLine();
+
+double n1 = 1.234;
+float n2 = 1.234F;
+decimal n3 = 1.234M;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+Console.ReadLine();
+
+Console.WriteLine("Comparando a precisão de float, double e decimal");
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadLine();
---
+++ TiposDeDados1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Declarando variaveis numericas");
+Console.WriteLine();
+//declarando variaveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -21472993;
+uint valor4 = 21472993;
+long valor5 = -9204930930;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+
+Console.ReadLine();
