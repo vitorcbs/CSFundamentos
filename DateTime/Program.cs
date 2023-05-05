fe75638d668af751b619e552b6257b046ddc8454
Vitinho <vitorcbs18@gmail.com>
2023-05-05 15:11:00 -0300

    DateTime

---
+++ DateTime/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Trabalhando com DateTime");
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+//criar data especifica
+
+DateTime dataHoje = new DateTime(2022, 05, 05);
+Console.WriteLine(dataHoje);
+
+DateTime dataHojeCompleta = new DateTime(2022, 05, 05, 14, 14, 00);
+Console.WriteLine(dataHojeCompleta);
+
+//extrair informações
+Console.WriteLine();
+Console.WriteLine("Extraindo informações");
+Console.WriteLine(dataAtual);
+
+Console.WriteLine(dataAtual.Year);
+Console.WriteLine(dataAtual.Month);
+Console.WriteLine(dataAtual.Day);
+Console.WriteLine(dataAtual.Minute);
+Console.WriteLine(dataAtual.Second);
+Console.WriteLine(dataAtual.Millisecond);
+
+//adicionando valores
+Console.WriteLine();
+Console.WriteLine("Adicionando valores");
+Console.WriteLine(dataAtual.AddDays(30));
+Console.WriteLine(dataAtual.AddMonths(3));
+Console.WriteLine(dataAtual.AddHours(10));
+Console.WriteLine(dataAtual.AddYears(100));
+
+//Obtendo dia da semana e do ano
+Console.WriteLine();
+Console.WriteLine("Obtendo dia da semana e do ano");
+Console.WriteLine(dataAtual.DayOfWeek);
+Console.WriteLine(dataAtual.DayOfYear);
+
+//Data no formato longo e curto
+Console.WriteLine();
+Console.WriteLine("Data no formato longo e curto");
+Console.WriteLine(dataAtual.ToLongDateString());
+Console.WriteLine(dataAtual.ToShortDateString());
+
+//Hora no formato longo e curto
+Console.WriteLine();
+Console.WriteLine("Hora no formato longo e curto");
+Console.WriteLine(dataAtual.ToLongTimeString());
+Console.WriteLine(dataAtual.ToShortTimeString());
+
+Console.ReadKey();  
--- TiposDeDados4/Program.cs
+++ TiposDeDados4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Definindo variáveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso C#";
+nome = "Isto é uma string alterada";
+
+Console.WriteLine(nome);
+
+object nota = 10;
+dynamic valor1 = 10.1m;
+
+Console.WriteLine(valor1);
+Console.WriteLine(nota);
+
+Console.ReadLine(); 
